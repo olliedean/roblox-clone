5b08fafc778a768f5e48b2392bdf7f692e35ef83
olliedean <olliedean@kakao.com>
2022-03-14 17:28:33 +0000

    ✨ Navbar and Searchbar frontend

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,13 @@
-import { faBars, faBurger, faIcons } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faBurger, faIcons, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import NavItem from "./NavItem";
 
 
 export default function Header() {
 
     return (
-        <div className="header flex p-1 px-3 items-center">
+        <div className="header flex px-3 items-center">
             <div className="flex-1 flex items-center">
                 <span className="icon-spritesheet icon-burger-menu"></span>
                 <Image
@@ -15,8 +16,25 @@ export default function Header() {
                     height={30}
                     className="ml-3"
                 />
+                <div className="pl-3 flex">
+                    <NavItem>Discover</NavItem>
+                    <NavItem>Avatar Shop</NavItem>
+                    <NavItem>Create</NavItem>
+                    <NavItem>Robux</NavItem>
+                </div>
+                <div className="ml-5">
+                    <FontAwesomeIcon icon={faSearch} className="absolute ml-2 mt-[6.5px] text-slate-300"/>
+                    <input
+                        className="bg-black bg-opacity-25 px-3 pl-9 text-lg placeholder:text-slate-500
+                            rounded border-[1px] border-slate-700 focus:border-slate-600"
+                        type="text"
+                        placeholder="Search"
+                    >
+                    
+                    </input>
+                </div>
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center py-1">
                 <span className="icon-spritesheet icon-robux"></span>
                 <span className="icon-spritesheet icon-settings"></span>
             </div>
---
+++ components/NavItem.js
@@ -0,0 +1,8 @@
+export default function NavItem({children}) {
+    return (
+        <div className="flex h-full items-center font-semibold px-4
+            bg-black bg-opacity-0 hover:bg-opacity-10">
+            {children}
+        </div>
+    )
+}
