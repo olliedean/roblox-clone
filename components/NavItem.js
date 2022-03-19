96fb52e15f3ddb04ecf719aba2fdd9a37f6ff8c4
olliedean <olliedean@kakao.com>
2022-03-19 13:06:13 +0000

    🐛 NavItem doesn't jump on hover anymore

--- components/NavItem.js
+++ components/NavItem.js
@@ -2,7 +2,7 @@ export default function NavItem({ children }) {
   return (
     <div className="flex h-full items-center font-medium px-4
             bg-black bg-opacity-0 hover:bg-opacity-10 pt-1
-            hover:border-b-2
+            hover:border-b-2 hover:pt-[6px]
             "
     >
       {children}
