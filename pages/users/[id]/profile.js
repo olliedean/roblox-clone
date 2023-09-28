0e9cf39b7cfe7224669126711ad64e0ac89e225d
olliedean <olliedean@kakao.com>
2023-09-29 00:59:13 +0100

    currently wearing added to profile

--- pages/users/[id]/profile.js
+++ pages/users/[id]/profile.js
@@ -3,8 +3,11 @@ import { roundArrow } from 'tippy.js';
 import 'tippy.js/dist/svg-arrow.css';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
+import ProfileAvatarItem from '../../../components/ProfileAvatarItem';
+import { useState } from 'react';
 
 export default function UserProfile() {
+  const [avatarPreviewMode, setAvatarPreviewMode] = useState('3D');
   return (
     <div className="max-w-5xl mx-auto mt-4">
       <NextSeo
@@ -61,7 +64,7 @@ export default function UserProfile() {
       <div className="mt-2">
         { /* changable content through tabs */ }
         <span className="text-xl font-bold">About</span>
-        <div className="text-gray-300 pb-5 border-b border-gray-400">
+        <div className="text-gray-300 pb-5 border-b border-neutral-500">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis
         </div>
         <div className="flex text-sm pt-2">
@@ -82,6 +85,39 @@ export default function UserProfile() {
           >
             Report Abuse
           </a>
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <span className="text-xl font-bold">Currently Wearing</span>
+        <div className="grid grid-cols-2 mt-2">
+          <div className="bg-neutral-900 h-[300px] flex flex-col">
+            <div className="max-w-lg w-full flex">
+              <button
+                type="button"
+                className="w-12 ml-auto mr-3 mt-3 p-2 bg-white rounded-md text-black font-medium"
+                onClick={() => setAvatarPreviewMode((avatarPreviewMode === '3D') ? '2D' : '3D')}
+              >
+                {(avatarPreviewMode === '3D') ? '2D' : '3D'}
+              </button>
+            </div>
+            <div className="3d-container h-full w-full" />
+          </div>
+          <div className="bg-neutral-700 py-7">
+            <div className="grid grid-cols-4 gap-4 px-5">
+              <ProfileAvatarItem />
+              <ProfileAvatarItem />
+              <ProfileAvatarItem />
+              <ProfileAvatarItem />
+              <ProfileAvatarItem />
+              <ProfileAvatarItem />
+              <ProfileAvatarItem />
+              <ProfileAvatarItem />
+            </div>
+            <div className="pt-2 flex items-center justify-center">
+              <span className="w-5 h-5 bg-gray-100 inline-block rounded-full" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
