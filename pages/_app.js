6cf6d8ec7d68df8afbf49d8df22b5afc3911e09f
olliedean <olliedean@kakao.com>
2023-09-28 09:32:06 +0100

    started on footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import ChatMenu from '../components/ChatMenu';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 import '../styles/globals.css';
@@ -11,8 +12,11 @@ function MyApp({ Component, pageProps }) {
       <Header />
       <div className="flex">
         <Sidebar />
-        <div className="overflow-y-auto overflow-x-hidden h-[calc(100vh-42px)] flex-1">
-          <Component {...pageProps} />
+        <div className="overflow-y-auto flex flex-col overflow-x-hidden h-[calc(100vh-42px)] flex-1">
+          <div className="flex-grow">
+            <Component {...pageProps} />
+          </div>
+          <Footer />
         </div>
       </div>
       <ChatMenu />
